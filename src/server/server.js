08266b30d4428cc523836b6805510a5f1f9dd004
TreyDettmer <dettmertrey@gmail.com>
2020-06-29 21:04:21 -0700

    Attempting to fix bug with Heroku app

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const socketio = require('socket.io');
 
 const Constants = require('../shared/constants');
 const Game = require('./game');
-const webpackConfig = require('../../webpack.dev.js');
+//const webpackConfig = require('../../webpack.dev.js');
 
 // Setup an Express server
 const app = express();
@@ -15,8 +15,8 @@ app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'development') {
   // Setup Webpack for development
-  const compiler = webpack(webpackConfig);
-  app.use(webpackDevMiddleware(compiler));
+  // const compiler = webpack(webpackConfig);
+  // app.use(webpackDevMiddleware(compiler));
 } else {
   // Static serve the dist/ folder in production
   app.use(express.static('dist'));
